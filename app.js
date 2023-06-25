2c4ae01e5057b8d5264c0a063ab2162453cd9da6
theRahaman <rahamansakh@gmail.com>
2023-06-25 10:56:51 +0530

    some changes

---
+++ app.js
@@ -0,0 +1,8 @@
+function add(a1, a2) {
+    console.log(typeof a1);
+    return a1 + a2;
+}
+var number1 = "bkbjjk";
+var number2 = 12;
+var result = add(number1, number2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add (a1:string, a2:number){
+    console.log(typeof a1)
+return a1 + a2;
+}
+const number1 = "bkbjjk";
+const number2 = 12;
+const result = add (number1, number2)
+console.log(result);
